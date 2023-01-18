1b5720efb13f3e0350261b29a9f2f2913c049be8
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-01-18 16:37:07 -0500

    Added header component and allowed addition of multiple lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import ListContainer from './components/ListContainer';
 
 export default function App() {
   return (
     <>
+      <Header />
       <ListContainer />
     </>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <h3>To Do</h3>
+    </header>
+  );
+}
